de2d1f0c5847e2fd96d81b81c557fe7ea28d09c9
gabrielronny <gabrielronny123@hotmail.com>
2023-08-19 16:02:06 -0300

    feat: change ttl time on dns zone

--- azure/dns-zone.tf
+++ azure/dns-zone.tf
@@ -7,13 +7,13 @@ resource "azurerm_dns_a_record" "criminix_live" {
     name                = "@"
     zone_name           = azurerm_dns_zone.dns_criminix.name
     resource_group_name = azurerm_resource_group.rg_prod.name
-    ttl                 = 300
+    ttl                 = 120
     records             = [ azurerm_public_ip.ip_public_vm-applications.ip_address ]
 }
 resource "azurerm_dns_a_record" "criminix_api" {
     name                = "api"
     zone_name           = azurerm_dns_zone.dns_criminix.name
     resource_group_name = azurerm_resource_group.rg_prod.name
-    ttl                 = 300
+    ttl                 = 120
     records             = [ azurerm_public_ip.ip_public_vm-applications.ip_address ]
 }
