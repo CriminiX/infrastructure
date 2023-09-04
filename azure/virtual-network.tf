278a4a5ee613584c73112c35996f02912b852151
gabrielronny <gabrielronny123@hotmail.com>
2023-09-04 00:48:48 -0300

    feat: allow db port

--- azure/virtual-network.tf
+++ azure/virtual-network.tf
@@ -96,3 +96,20 @@ resource "azurerm_network_security_rule" "allow_ssh" {
 
     depends_on = [ azurerm_network_security_group.nsg_prod ]
 }
+
+resource "azurerm_network_security_rule" "allow_db" {
+    name                       = "allow-db-port"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+
+    resource_group_name         = azurerm_resource_group.rg_prod.name
+    network_security_group_name = azurerm_network_security_group.nsg_prod.name
+
+    depends_on = [ azurerm_network_security_group.nsg_prod ]
+}
