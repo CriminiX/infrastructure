a0d4f6ef4b8b9abb1a0475810cdacd56dc4bf54a
gabrielronny <gabrielronny123@hotmail.com>
2023-09-10 23:17:57 -0300

    feat: dns validation added and upgrade vm

--- azure/dns-zone.tf
+++ azure/dns-zone.tf
@@ -7,13 +7,26 @@ resource "azurerm_dns_a_record" "criminix_live" {
     name                = "@"
     zone_name           = azurerm_dns_zone.dns_criminix.name
     resource_group_name = azurerm_resource_group.rg_prod.name
-    ttl                 = 120
+    ttl                 = 300
     records             = [ azurerm_public_ip.ip_public_vm-applications.ip_address ]
 }
 resource "azurerm_dns_a_record" "criminix_api" {
-    name                = "api"
+    name                = "*"
     zone_name           = azurerm_dns_zone.dns_criminix.name
     resource_group_name = azurerm_resource_group.rg_prod.name
-    ttl                 = 120
+    ttl                 = 300
     records             = [ azurerm_public_ip.ip_public_vm-applications.ip_address ]
+}
+
+
+
+resource "azurerm_dns_txt_record" "criminix_api" {
+    name                = "_acme-challenge"
+    zone_name           = azurerm_dns_zone.dns_criminix.name
+    resource_group_name = azurerm_resource_group.rg_prod.name
+    ttl                 = 10
+
+    record {
+        value = "ZFHI7AIc8yNYiXKvM8FWxk_gkTSGdozNrfTmDDuVthw"
+    }
 }
--- azure/virtual-machine.tf
+++ azure/virtual-machine.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "vm_applications_prod" {
     location              = azurerm_resource_group.rg_prod.location
     resource_group_name   = azurerm_resource_group.rg_prod.name
     network_interface_ids = [ azurerm_network_interface.nic_vm_applications.id ]
-    size                  = "Standard_B2s"
+    size                  = "Standard_B2ms"
 
     admin_username = "admincriminix"
     disable_password_authentication = true
